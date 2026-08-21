6721aec2cd1cd301bcf0a1ea43f3c690f4b36be6
sunooni <sunooniiii@gmail.com>
2026-08-20 22:33:47 -0700

    Fix Header CSS import path

--- src/features/search/ui/Search.tsx
+++ src/features/search/ui/Search.tsx
@@ -2,7 +2,7 @@ import { SearchOutlined } from "@ant-design/icons";
 import { useSearchModal } from "../model/useSearchModal";
 import { SearchModal } from "./SearchModal";
 
-import styles from "@/widgets/Header/ui/Header.module.css";
+import styles from "@/widgets/header/ui/Header.module.css";
 
 export const Search = () => {
   const { isOpen, open, close } = useSearchModal();
